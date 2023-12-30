bfd0c023ec0e454287d099fa8cd7f2fb0353e77e
Naishalmodi <94700496+Naishalmodi@users.noreply.github.com>
2023-12-30 14:05:35 +0530

    hello world

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Welcomescreen from './screens/welcomescreen';
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// import Welcomescreen from './screens/welcomescreen';
 
 // import Stacknavigator from './Stacknavigator';
 
 export default function App() {
 
-    const Stack = createNativeStackNavigator();
+    // const Stack = createNativeStackNavigator();
     return (
-      <>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={Welcomescreen} />
-          </Stack.Navigator>
-        </NavigationContainer>  </>
+      <View>
+        <Text>
+          Hello Worlds
+        </Text>
+      </View>
+        
     );
   }
 
